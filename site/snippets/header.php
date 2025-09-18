ef9c5bc9a42fdb3980986211bd2b1351b4d3576f
supergijs <info@supergijs.com>
2025-09-18 17:44:05 +0200

    update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,7 @@
 <header>
     <?php if ($image = $site->header_image()->toFile()): ?>
-        <img src="<?= $image->url() ?>" alt="<?= $image->alt() ?>">
-        <?php endif ?>
+    <img src="<?= $image->url() ?>" alt="<?= $image->alt()->or('Site image') ?>">
+    <?php endif ?>
     <h1><?= $site->title() ?></h1>
     <h2><?= $site->subtitle() ?></h2>
     <h3><?= $site->intro() ?></h3>
